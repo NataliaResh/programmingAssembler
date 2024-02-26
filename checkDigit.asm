a17368cf21b6695aa3dc4449052077e804f47b60
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-02-26 16:26:59 +0700

    Solve check digit task

---
+++ checkDigit.asm
@@ -0,0 +1,29 @@
+.text
+.macro syscall %n
+ 	li a7, %n
+ 	ecall
+.end_macro
+
+.macro exit %ecode
+	li a0, %ecode
+	syscall 93
+.end_macro
+
+.macro readch
+	syscall 12
+.end_macro
+
+.macro printch
+	syscall 11
+.end_macro
+
+main:
+	readch
+	andi s0 a0, 0xff
+	li a0, 10
+	printch
+	addi a0, s0, -48
+	sltiu a0, a0, 10
+	addi a0, a0, 48
+	printch
+	exit 0
